2d4efa38e8547b235198e86b7d41318b7465d715
Luiz Guilherme <lu.costa1402@gmail.com>
2026-09-22 23:01:14 -0300

    Criando um triangulo com loop for

--- nested_loops/retangulo_triangulo.py
+++ nested_loops/retangulo_triangulo.py
@@ -6,3 +6,11 @@ for i in range(altura):
     for j in range(largura):
         print("*", end=" ")
     print()
+
+# Triangulo com FOR
+altura = 5
+
+for i in range(altura):
+    espacos = altura - i - 1
+    estrelas = 2 * i + 1
+    print(" " * espacos + "*" * estrelas)
